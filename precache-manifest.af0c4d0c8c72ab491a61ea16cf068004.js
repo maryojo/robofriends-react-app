79e21ddda10f14e185da87c8548059738bdc5bad
Mary Ojo <maryojo3@gmail.com>
2020-06-27 03:47:13 +0100

    Updates

---
+++ precache-manifest.af0c4d0c8c72ab491a61ea16cf068004.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a16ce5e0a62b8b5c1a68538cc485a82",
+    "url": "/oebunoluwa/robofriends-react-app/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/oebunoluwa/robofriends-react-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "82c28283ac7f80255242",
+    "url": "/oebunoluwa/robofriends-react-app/static/css/main.7ae8d151.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/oebunoluwa/robofriends-react-app/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oebunoluwa/robofriends-react-app/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82c28283ac7f80255242",
+    "url": "/oebunoluwa/robofriends-react-app/static/js/main.69c0e13e.chunk.js"
+  },
+  {
+    "revision": "d3c47c2a11ff859ea6a2",
+    "url": "/oebunoluwa/robofriends-react-app/static/js/runtime-main.68c712c5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/oebunoluwa/robofriends-react-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
